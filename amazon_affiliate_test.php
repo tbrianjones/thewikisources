2f1448feafe548f5cc7de7d6aa6e37369ce62485
T. Brian Jones <tbrianjones@gmail.com>
2013-01-23 07:52:40 +0000

    added affiliate text page.

---
+++ amazon_affiliate_test.php
@@ -0,0 +1,24 @@
+<html>
+
+	<head>
+	</head>
+	
+	<body>	
+		
+		<h2>testing amazon affiliate link</h2>
+		<p>some text about this book</p>
+
+		<br />
+		<iframe src="http://rcm.amazon.com/e/cm?lt1=_top&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=induinteinc-20&o=1&p=8&l=as1&m=amazon&f=ifr&ref=tf_til&asins=0914076728" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+	
+		<br />
+		<a href="http://www.amazon.com/gp/product/0914076728/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0914076728&linkCode=as2&tag=induinteinc-20">Germantown and the Germans: An Exhibition . . . from the Collection of the Library Company of Philadelphia and the Historical Society of Pennsylvania</a><img src="http://www.assoc-amazon.com/e/ir?t=induinteinc-20&l=as2&o=1&a=0914076728" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+		
+		<br />
+		<script type="text/javascript" src="http://wms.assoc-amazon.com/20070822/US/js/link-enhancer-common.js?tag=induinteinc-20"></script>
+		<noscript><img src="http://wms.assoc-amazon.com/20070822/US/img/noscript.gif?tag=induinteinc-20" alt="" /></noscript>
+
+	</body>
+
+
+</html>
