c9daa8d0addc6a80b13c44361ef74df0c89c58d0
T. Brian Jones <tbrianjones@gmail.com>
2013-01-24 18:35:34 +0000

    added brief aprser that writes correctly to mysql

--- wiki_article_model.php
+++ wiki_article_model.php
@@ -3,19 +3,27 @@
 	class Wiki_article_model
 	{
 		
+		
+		// settings
+		private $brief_length = 1; // number of paragraphs to grab from the begining of the article to use as brief
+		
+		
+	// --- DO NOT EDIT SETTINGS BELOW THIS LINE -----------------------------------
+		
+		
 		// mysql connection
 		private $mysqli;
 		
 		// class attributes
-		private $title;		// article title ( unique identifier on wikipedia )
-		private $url;		// article's url on wikipedia
-		private $html;		// raw html of the article
+		public $title;		// article title ( unique identifier on wikipedia )
+		public $url;		// article's url on wikipedia
+		public $html;		// raw html of the article
 		
 		// extracted article data
-		private $brief;		// first section of a wikipedia article
-		private $books = array();
-		private $book_references = array();
-		private $date_references = array();
+		public $brief;		// first section of a wikipedia article
+		public $books = array();
+		public $book_references = array();
+		public $date_references = array();
 		
 		// dom stuff
 		private $Dom;
@@ -27,8 +35,14 @@
 			$title	// the article title to fetch
 		) {
 			
+			// set default timezone
+			date_default_timezone_set('America/Los_Angeles');
+			
 			// connect to mysql
 			$this->Mysqli = new mysqli( 'wikipedia.cw0tm7tgwtd4.us-east-1.rds.amazonaws.com', 'jones', 'zMfZdhce', 'wikipedia' );
+			
+			// set mysql connection to use utf-8
+			$this->Mysqli->set_charset( 'utf8' );
 			
 			// create data with passed values
 			$this->title = $title;
@@ -37,6 +51,8 @@
 			// extract data from html
 			$this->get_html();
 			$this->get_brief();
+			
+			
 			
 			// write data to database
 			$this->update_article();
@@ -50,7 +66,10 @@
 		// retrieve the webpage from wikipedia.org
 		//
 		private function get_html() {
-
+			
+			// sleep so we don't crush wikipedia
+			//sleep( 1 );
+			
 			// download page
 			$this->html = file_get_contents( $this->url );
 			
@@ -64,12 +83,13 @@
 
 		private function get_brief()
 		{
+		
 			$i = 0;
 			$this->brief = '';
 			$ps = $this->Body->getElementsByTagName( 'p' );
 			foreach( $ps as $p ) {
-				if( $i < 3 )
-					$this->brief .= "\n\n" . $p->nodeValue;
+				if( $i < $this->brief_length )
+					$this->brief .= "\n" . $p->nodeValue;
 				else
 					break;
 				$i++;
@@ -87,7 +107,10 @@
 		{
 			
 			$sql = "UPDATE articles
-					SET brief = '" . $this->Mysqli->real_escape_string( $this->brief ) . "'
+					SET
+						brief = '" . $this->Mysqli->real_escape_string( $this->brief ) . "',
+						last_retrieved = '" . date( 'c' ) . "'
+						
 					WHERE title = '" . $this->Mysqli->real_escape_string( $this->title ) . "'";
 			$response = $this->Mysqli->query( $sql );
 			
